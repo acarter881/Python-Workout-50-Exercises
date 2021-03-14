514467710fb6fafd4430c5cbe4d955e698e1c54f
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-03-14 14:30:14 -0500

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,19 @@
+#! python3
+
+"""
+nobody:*:-2:-2::0:0:Unprivileged User:/var/empty:/usr/bin/false
+root:*:0:0::0:0:System Administrator:/var/root:/bin/sh
+daemon:*:1:1::0:0:System Services:/var/root:/usr/bin/false
+"""
+
+def passwd_to_dict(fileName):
+    usernames = {}
+    with open(fileName, 'r') as f:
+        for row in f.readlines():
+            if row.strip() == '' or row.startswith('#'):
+                pass
+            else:
+                usernames[row.split(':')[0]] = row.split(':')[2]
+    return usernames
+
+print(passwd_to_dict('./Python/unixStyle.txt'))
