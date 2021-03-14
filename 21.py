e8601695ffc8a562f41935ae6953146af9953c90
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-03-14 16:51:54 -0500

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,19 @@
+#! python3
+import os
+
+def find_longest_word(fileName):
+    maxLength = 0
+    theWord = ''
+    with open(fileName, 'r') as f:
+        for row in f:
+            for word in row.split():
+                if len(word) > maxLength:
+                    maxLength = len(word)
+                    theWord = word
+    return theWord
+
+def find_all_longest_words(directory):
+    for fileName in os.listdir(directory):
+        print(find_longest_word(fileName))
+
+find_all_longest_words('.')
