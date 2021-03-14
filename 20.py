1e5a8a2973b5ab752bbb4b489dc9ac30b15c49ee
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-03-14 15:33:39 -0500

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,18 +1,22 @@
 #! python3
 
 def wordcount(fileName):
-    with open(fileName, 'r') as f:
-        numberOfCharacters = len(f.read())
-    with open(fileName, 'r') as f:
-        numberOfWords = len(f.read().split())
-    with open(fileName, 'r') as f:
-        numberOfLines = len(f.readlines())
+    numberOfCharacters = 0
+    numberOfWords = 0
+    numberOfLines = 0
+    numberOfUniqueWords = 0
+
     with open(fileName, 'r') as f:
         eL = []
-        for word in f.read().split():
-            if word not in eL:
-                eL.append(word)
-        numberOfUniqueWords = len(eL)
+        for row in f:
+            numberOfCharacters += len(row)
+            numberOfWords += len(row.split())
+            numberOfLines += 1
+            for word in row.split():
+                if word not in eL:
+                    eL.append(word)
+                    numberOfUniqueWords += 1
+     
     return numberOfCharacters, numberOfWords, numberOfLines, numberOfUniqueWords
 
 print(wordcount('./Python/unixStyle.txt'))
