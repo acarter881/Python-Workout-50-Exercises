53828546a9abab7584dbf5fadca124ff69a6d05a
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-03-27 15:13:51 -0500

    Update 26.py

--- 26.py
+++ 26.py
@@ -19,7 +19,7 @@ def calc(mathString):
                   '**': 'pow',
     }
     method_to_call = getattr(operator, operations[mathString[0:2].strip()])
-    return method_to_call(int(mathString[2:4].strip()),int(mathString[4:].strip()))
+    return method_to_call(int(mathString[2:4].strip()), int(mathString[4:].strip()))
 
 print(calc('% 2 3'))
 print(calc('** 4 3'))
