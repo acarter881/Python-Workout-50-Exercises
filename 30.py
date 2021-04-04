568efc7d2718cf9dd6222f38d3dbf57441c68264
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-04-04 16:51:53 -0500

    Update 30.py

--- 30.py
+++ 30.py
@@ -1,8 +1,6 @@
 #! python3
 
 def flatten(userList):
-    newList = []
-    [newList.append(j) for i in userList for j in i]
-    return newList
+    return [j for i in userList for j in i]
 
 print(flatten([[1,2], [3,4]]))
