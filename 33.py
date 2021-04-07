236520d4c8755d57b4cc5aacf695279bd21b03da
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-04-06 23:30:33 -0500

    Create 33.py

---
+++ 33.py
@@ -0,0 +1,10 @@
+#! python3
+
+def transform_values(func, user_dict):
+    return {k:func(v) for k,v in user_dict.items()}
+
+
+# Goal is to return {'a': 1, 'b': 4, 'c': 9}
+d = {'a':1, 'b':2, 'c':3}
+
+print(transform_values(lambda x: x*x, d))
