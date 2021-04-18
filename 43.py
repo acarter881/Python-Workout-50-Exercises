20c5505772fb2ee627fe0030c3633e2ff35f3c2b
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-04-18 15:27:05 -0500

    Update 43.py

--- 43.py
+++ 43.py
@@ -1,37 +1,36 @@
 #! python3
 
-class Sheep:
-    def __init__(self, color):
+class Animal():
+    def __init__(self, color, number_of_legs):
+        self.species = self.__class__.__name__
         self.color = color
-        self.species = 'sheep'
-        self.number_of_legs = 4
-        
+        self.number_of_legs = number_of_legs
+
     def __repr__(self):
-        return str([self.color, self.species, self.number_of_legs])
+        return f'{self.color} {str(self.species).lower()}, {self.number_of_legs} legs'
 
-class Wolves(Sheep):
+
+class Sheep(Animal):
     def __init__(self, color):
-        self.color = color
-        self.species = 'wolf'
-        self.number_of_legs = 4
+        super().__init__(color, 4)
 
-class Snakes(Sheep):
+class Wolf(Animal):
     def __init__(self, color):
-        self.color = color
-        self.species = 'snake'
-        self.number_of_legs = 0
+        super().__init__(color, 4)
 
-class Parrots(Sheep):
+class Snake(Animal):
     def __init__(self, color):
-        self.color = color
-        self.species = 'parrot'
-        self.number_of_legs = 2
+        super().__init__(color, 0)
 
-s = Sheep('white')
-print(s)
-w = Wolves('gray')
-print(w)
-k = Snakes('green')
-print(k)
-p = Parrots('purple')
-print(p)
+class Parrot(Animal):
+    def __init__(self, color):
+        super().__init__(color, 2)
+
+sheep = Sheep('white')
+print(sheep)
+wolf = Wolf('gray')
+print(wolf)
+snake = Snake('green')
+print(snake)
+parrot = Parrot('purple')
+print(parrot)
