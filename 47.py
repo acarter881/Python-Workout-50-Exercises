b2b04e666e497b4530af3b2573a9aac1e805ebc1
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-04-18 20:54:58 -0500

    Create 47.py

---
+++ 47.py
@@ -0,0 +1,29 @@
+#! python3
+
+class CircleIterator:
+    def __init__(self, sequence, number):
+        self.sequence = sequence
+        self.number = number
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= self.number:
+            raise StopIteration
+
+        value = self.sequence[self.index % len(self.sequence)]
+        self.index += 1
+        return value
+
+class Circle:
+    def __init__(self, sequence, number):
+        self.sequence = sequence
+        self.number = number
+    
+    def __iter__(self):
+        return CircleIterator(self.sequence, self.number)
+    
+c = Circle('abc', 6)
+print(list(c))
+
+d = Circle('abcd', 7)
+print(list(d))
