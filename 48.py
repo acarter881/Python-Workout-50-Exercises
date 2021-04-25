76a7dbd195e563c3914981783e5cf55fd052028c
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-04-25 15:28:15 -0500

    Update 48.py

--- 48.py
+++ 48.py
@@ -2,13 +2,11 @@
 import os
 
 def daGenerator(dirName):
-    os.chdir(dirName)
     for textFile in os.listdir(dirName):
-        for row in open(textFile, 'r'):
+        for row in open(os.path.join(dirName, textFile), 'r'):
             yield row.split('\n')[0]
 
 nextGen = daGenerator('C:\\Users\\Alex\\Desktop\\Test')
-print(next(nextGen))
 print(next(nextGen))
 print(next(nextGen))
 print(next(nextGen))
